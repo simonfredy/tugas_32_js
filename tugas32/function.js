346ecc5d9410cb16171b212adacbfbf6f051431e
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-30 21:16:07 +0700

    Tugas 32 JS

---
+++ tugas32/function.js
@@ -0,0 +1,6 @@
+function tugas() {
+    console.log(document.getElementById('nDepan'));
+    console.log(document.getElementById('nBelakang'));
+    console.log(document.getElementById('hobi'));
+    
+}
